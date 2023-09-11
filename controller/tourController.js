3d813bfd279b1b55034e15dbf1cba0f0c4031305
Tambua Evaristus <evaristustambua@gmail.com>
2023-09-12 00:01:11 +0100

    sorting check

--- controller/tourController.js
+++ controller/tourController.js
@@ -3,11 +3,6 @@ const Tour = require("./../model/tourModel");
 exports.createTour = async (req, res) => {
   try {
     const newTour = await Tour.create(req.body);
-    const queryObj = { ...req.query };
-
-    const excludedFields = ["pages", "sort", "limit", "fields"];
-    excludedFields.forEach((field) => delete queryObj[field]);
-
     res.status(201).json({
       status: "success",
       data: {
@@ -24,14 +19,26 @@ exports.createTour = async (req, res) => {
 
 exports.getTours = async (req, res) => {
   try {
-    console.log(req.query);
-    // const tours = await Tour.find(
-    //   {
-    //     rating: req.query.rating,
-    //   }
-    // );
-    const tours = await Tour.find().where("rating").equals(req.query.rating);
+    const queryObj = { ...req.query };
 
+    // Filtering
+
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((field) => delete queryObj[field]);
+
+    let queryStr = JSON.stringify(queryObj);
+
+    // queryStr = queryStr.replace(/\b(gte|lte|lt|gt)\b/g, (match) => `$${match}`);
+
+    console.log(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // Sorting
+    if (req.query.sort) {
+      query = query.sort(req.query.sort);
+    }
+
+   const tours = await query
     res.status(200).json({
       status: "success",
       data: {
