4fda7f5aac3de30b299b7f85c971d0a7379c3cf4
Tambua Evaristus <evaristustambua@gmail.com>
2023-09-26 16:02:32 +0100

    hash password with bcrypt

--- model/userModel.js
+++ model/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -21,9 +22,22 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, "Password is required"],
-    minlength: 8,
+    validate: {
+      validator(el) {
+        return el === this.password;
+      },
+      message: "passwords are not the same",
+    },
   },
 });
-const user = mongoose.model("User", userSchema);
 
-module.exports = user;
+userSchema.pre("save", async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next(); 
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
