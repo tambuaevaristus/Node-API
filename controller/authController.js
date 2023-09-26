1fd976d5a88938bea4801815d0d97ee9f3ce4568
Tambua Evaristus <evaristustambua@gmail.com>
2023-09-26 17:30:42 +0100

    jwt signup

--- controller/authController.js
+++ controller/authController.js
@@ -1,14 +1,31 @@
+const jwt = require("jsonwebtoken");
 const User = require("./../model/userModel");
 const catchAsync = require("./../utils/catchAsync");
 
-exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+exports.signup = async (req, res, next) => {
+  try {
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm,
+    });
 
-  res.status(201).json({
-    status: "success",
-    data: {
-      user: newUser,
-    },
-  });
-});
- 
+    const token = jwt.sign({ id: newUser._id }, "secret-key", {
+      expiresIn: "90d",
+    });
+
+    res.status(201).json({
+      status: "success",
+      token,
+      data: {
+        user: newUser,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
