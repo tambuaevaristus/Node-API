650e736a21de4b34c6b754263fb201e6c59cf0a2
Tambua Evaristus <evaristustambua@gmail.com>
2023-11-30 14:50:24 +0100

    verification of phone number

--- controller/messageController.js
+++ controller/messageController.js
@@ -5,16 +5,62 @@ const authToken = "d675f2fd87eee1965fd965ced472a4a6";
 const client = require("twilio")(accountSid, authToken);
 
 exports.sendMessage = catchAsync(async (req, res, next) => {
-  const messageResponse = await client.messages.create({
-    body: req.body.body,
-    from: "whatsapp:+14154668513",
-    to: "whatsapp:" + req.body.to,
+  const message = req.body.body;
+  const phoneNumber = req.body.to;
+
+  send(phoneNumber, message);
+
+  const response = await client.validationRequests.create({
+    friendlyName: "WhatsApp Number Validation",
+    phoneNumber: `${phoneNumber}`,
   });
 
-  res.status(200).json({
-    status: "success",
-    data: {
-      messageResponse,
-    },
-  });
+  if (response.valid) {
+    res.status(200).json({
+      status: "success",
+      data: {
+        response,
+      },
+    });
+  } else {
+    // return `Phone number ${phoneNumber} does not exist on WhatsApp.`;
+    res.status(400).json({
+      status: "failed",
+      data: {
+        response,
+      },
+    });
+  }
 });
+
+// async function checkWhatsAppNumber(phoneNumber) {
+//   try {
+//   } catch (error) {
+//     console.error("Error checking WhatsApp number:", error);
+//   }
+// }
+
+async function send(phone, body) {
+  const messageResponse = await client.messages
+    .create({
+      body: body,
+      from: "whatsapp:+14154668513",
+      to: "whatsapp:" + phone,
+    })
+    .then((response) => {
+      res.status(200).json({
+        status: "success",
+        data: {
+          response,
+        },
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        status: "failed",
+        data: {
+          error,
+        },
+      });
+    });
+}
